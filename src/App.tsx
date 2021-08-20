4ed7785994c8780b8563c47f8341047fd756bf30
danial <dev.salahi@gmail.com>
2021-08-20 11:59:18 +0300

    added tasks component to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Tasks from './components/Tasks'
 
 function App() {
   return (
     <main className="container">
+      <div className="row">
+        <div className="col-10 mx-auto p-5">
+          <h2 className="text-dark text-center">Task Manager</h2>
+          <Tasks />
+        </div>
+      </div>
     </main>
   );
 }
